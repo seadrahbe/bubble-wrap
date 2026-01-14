255ba0071ac25639ad3fa4b6b6730a58a978ed57
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-01-14 03:04:30 +0000

    Added A press functionality to change color to black

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
 
 
 int main() {
     bn::core::init();
     bn::backdrop::set_color(bn::color(28, 10, 17));
 
+    if (bn::keypad::a_pressed()) {
+    bn::backdrop::set_color(bn::color(5, 5, 5));
+  }
+
     while(true) {
+      
+      if (bn::keypad::a_pressed()) {
+        bn::backdrop::set_color(bn::color(5, 5, 5));
+      }
+
     bn::core::update();
   }
 }
