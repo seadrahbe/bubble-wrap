80aea33ee1f1f24a928d56e557f15df12ff9824c
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-01-19 22:15:05 +0000

    Line of yellow sprites no loops yet

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,28 @@ int main() {
     bn::core::init();
     bn::backdrop::set_color(bn::color(28, 10, 17));
 
-    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(50, 40);
-    bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-50, 40);
-    bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(50, -40);
-    bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-50, -40);
-    bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(25, 20);
-    bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(-25, -20);
-    bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(0, 0);
+    // Sprite art
+
+    // bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(50, 40);
+    // bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-50, 40);
+    // bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(50, -40);
+    // bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-50, -40);
+    // bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(25, 20);
+    // bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(-25, -20);
+    // bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(0, 0);
+
+    // Brute force
+
+  bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(-40, 40);
+  bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-30, 40);
+  bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-20, 40);
+  bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-10, 40);
+  bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(0, 40);
+  bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(10, 40);
+  bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(20, 40);
+  bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(30, 40);
+  bn::sprite_ptr myCircle9 = bn::sprite_items::dot.create_sprite(40, 40);
+
 
     if (bn::keypad::a_pressed()) {
     bn::backdrop::set_color(bn::color(5, 5, 5));
